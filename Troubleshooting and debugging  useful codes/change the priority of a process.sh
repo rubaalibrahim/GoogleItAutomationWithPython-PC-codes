20c8be68513be199be8cb5a97e8d9ab4dc769699
Ruba Al Ibrahim <47699222+rubaalibrahim@users.noreply.github.com>
2021-03-27 21:42:10 +0300

    Delete Troubleshooting and debugging  useful codes directory

--- Troubleshooting and debugging  useful codes/change the priority of a process.sh
+++
@@ -1,5 +0,0 @@
-''' bash script to automate the task of changing the priority of processes that are running synchronously (for example ffmpeg) and are taking up too much RAM and slowing down the server, i used pidof command to get the PID and then changed the priority of the processes to 19 which is the lowest priority by using the renice command
-
-for pid in $(pidof ffmpeg);
-do renice 19 $pid;
-done
